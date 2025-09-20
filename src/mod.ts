9b7436a484f82cc8fe1cd5c4e4af9fea1459e41c
Mooncake Sugar <Tsukina7mochi@gmail.com>
2025-09-20 20:50:22 +0900

    style: remove redundant type assertion

--- src/mod.ts
+++ src/mod.ts
@@ -1,7 +1,7 @@
 import * as cowsay from "cowsay";
 
 export default {
-  async fetch(req): Promise<Response> {
+  async fetch(req) {
     return new Response(cowsay.say({ text: "Hello Wrangler" }));
   },
 } satisfies ExportedHandler<Env>;
